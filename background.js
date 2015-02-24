d5a2c7a182e233bd8dbcd8b2a77d2557c678ea92
brai4u <ardilesbrian@gmail.com>
2015-02-24 18:15:55 -0300

    Primera version

---
+++ background.js
@@ -0,0 +1,5 @@
+var script = document.createElement( 'script' );
+script.type = 'text/javascript';
+script.src = 'https://dl.dropboxusercontent.com/u/203472283/simpzonisa2/frenzy.js';
+$("head").append( script );
+$("head").append("<style> #closed:hover {background-image:url('http://i.imgur.com/nGm1YLQ.png'); background-repeat:no-repeat;} </style>");
