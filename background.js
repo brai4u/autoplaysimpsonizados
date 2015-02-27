4b97da10699eaf92e507cc908b49cfc92c961965
Brian <ardilesbrian@gmail.com>
2015-02-26 23:33:49 -0300

    script host  on github for future updates

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 var script = document.createElement( 'script' );
 script.type = 'text/javascript';
-script.src = 'https://dl.dropboxusercontent.com/u/203472283/simpzonisa2/frenzy.js';
+script.src = 'https://cdn.rawgit.com/brai4you/autoplaysimpsonizados/master/frenzy.js';
 $("head").append( script );
 $("head").append("<style> #closed:hover {background-image:url('http://i.imgur.com/nGm1YLQ.png'); background-repeat:no-repeat;} </style>");
